e52db7dfde66faca9b5b50d8ad30156ad157a7e4
joaobssk <joao.1309@gmail.com>
2018-02-17 19:12:39 -0300

    início dos trabalhos

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+    
+    <head>
+        <meta charset="UTF-8">
+        <title>KnowQui</title>
+        <link rel="icon" href="https://png.icons8.com/dusk/50/000000/test-tube.png" type="image/x-icon">
+        <link rel="stylesheet" href="css/bootstrap.css"> 
+        <link rel="stylesheet" href="css/knowqui.css">
+    </head>
+    
+    <body>
+        
+        <div class="container">
+        
+            <div class ="row">
+            
+                <div class="col-sm-4 col-sm-offset-4 login">
+
+                    <input type="text" placeholder="TESTE">
+
+                </div>
+    
+            </div>
+        </div>
+        
+        <footer>
+        
+        </footer>
+    </body>
+    
+</html>
