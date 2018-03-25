86f678416ca4fd2d6140b254bb433bacbee647fa
DESKTOP-8HANQF2\leand <leandro.aso123@outlook.com>
2018-03-24 22:27:48 -0300

    no message

--- app/Http/Controllers/TipoPerguntaController.php
+++ app/Http/Controllers/TipoPerguntaController.php
@@ -7,7 +7,7 @@ class TipoPerguntaController extends Controller{
 
 	public function lista(){
 		if(Auth::check()){
-			$tiposPerguntas = DB::select('select id, descricao from TipoPergunta');
+			$tiposPerguntas = DB::select('select id, descricao from tipopergunta');
 			return view('pergunta/cadastro-tipo-pergunta')->with('tiposPerguntas',$tiposPerguntas);
 		}else{
 			return redirect('/home');
